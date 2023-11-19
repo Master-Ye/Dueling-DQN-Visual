e8254d56284c2a8277efb08d69d2dbb4527c9944
masterder <1813708165@qq.com>
2023-11-19 10:39:01 +0800

    11.18

--- src/views/EpochView.jsx
+++ src/views/EpochView.jsx
@@ -4,106 +4,112 @@ import styles from "../style/viewStyle/EpochView.module.sass";
 import EchartsComponents from "@/components/EchartsComponent";
 import { useMemo } from "react";
 import { Input } from "@nextui-org/react";
-import { actionNum ,rewardNum} from "@/data/statistic";
+import { actionNum, rewardNum } from "@/data/statistic";
 import { useState } from "react";
 function EpochView() {
-  const [ Index, setIndex ] = useState(1);
-  const options = {
-    title: {
-      text: "Reward",
-      x: "34%",
-    },
-    legend: {
-      top: "10%",
-      textStyle: {},
-      show: false,
-    },
-    tooltip: {
-      trigger: "item",
-      show: true,
-    },
-    color: ["#7cc380", "white", "#81acda"],
-    series: [
-      {
-        name: "",
-        type: "pie",
-        radius: ["15%", "70%"], //大小修改
-        avoidLabelOverlap: false,
-        label: {
-          show: false,
-        },
-        emphasis: {
-          label: {
-            show: false,
-          },
-        },
-        labelLine: {
-          show: false,
-        },
-        itemStyle: {
-          borderWidth: 1, // 外边框的宽度
-          borderColor: "black", // 外边框的颜色
-          borderType: "solid", // 外边框的样式，默认为'solid'
-        },
-        data: [
-          { value: 580, name: "未开始", label: { color: "#dffaef" } },
-          { value: 735, name: "已完成", label: { color: "#1be8df" } },
-          { value: 1048, name: "进行中", label: { color: "#fbd408" } },
-        ],
+  const [Index, setIndex] = useState(1);
+  const sLineChartOption = (name, color, data, index = 0, nameArray) => {
+    const lineOption = {
+      title: {
+        text: name,
       },
-    ],
+      tooltip: {
+        trigger: "axis",
+      },
+      legend: {
+        data: nameArray,
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          name: nameArray[0],
+          type: "line",
+          stack: "Total",
+          data: data[index][0],
+          color:color[0]
+        },
+        {
+          name: nameArray[1],
+          type: "line",
+          stack: "Total",
+          data: data[index][1],
+          color:color[1]
+        },
+        {
+          name: nameArray[1],
+          type: "line",
+          stack: "Total",
+          data: data[index][2],
+          color:color[2]
+        },
+      ],
+    };
+    return lineOption;
   };
   const option = {
     title: {
-      text: "",
-    },
-    grid: {
-      x: 50,
-      y: 30,
-      x2: 0,
-      y2: 20,
+      text: 'Stacked Line'
     },
     tooltip: {
-      trigger: "axis",
+      trigger: 'axis'
     },
     legend: {
-      show: false,
-      itemWidth: 15,
-      itemHeight: 15,
-      data: ["可用", "不可用"],
+      data: ['Email', 'Union Ads', 'Video Ads']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
     },
     xAxis: {
-      data: ["网络设备", "服务器", "应用", "其他", "虚拟机", "存储"],
-      splitLine: {
-        show: false,
-      },
+
     },
     yAxis: {
-      type: "value",
-      name: "action_dis",
+      type: 'value'
     },
     series: [
       {
-        barWidth: 50,
-        barCategoryGap: "0%",
-        name: "可用",
-        type: "bar",
-        stack: "使用情况",
-        data: [5, 20, 36, 10, 10, 20],
-        itemStyle: {
-          normal: { color: "#FF8849" },
-        },
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 132, 101, 134, 90, 230, 210]
       },
       {
-        name: "不可用",
-        type: "bar",
-        stack: "使用情况",
-        data: [30, 22, 18, 35, 30, 30],
-        itemStyle: {
-          normal: { color: "#3FBB49" },
-        },
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [220, 182, 191, 234, 290, 330, 310]
       },
-    ],
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        data: [150, 232, 201, 154, 190, 330, 410]
+      },
+
+    ]
   };
   const sPineChartOption = (name, color, data, index = 0, nameArray) => {
     return {
@@ -115,7 +121,7 @@ function EpochView() {
         top: "10%",
         textStyle: {},
         show: false,
-        z:1000,
+        z: 1000,
       },
       tooltip: {
         trigger: "item",
@@ -140,23 +146,21 @@ function EpochView() {
             show: false,
           },
           label: {
-            formatter:function(e){
-              let data=e.data;
-                if(data.value==0){
+            formatter: function (e) {
+              let data = e.data;
+              if (data.value == 0) {
+                data.label.show = false;
+              } else {
+                return `${data.name}`;
+              }
+            },
+            show: true,
+            position: "inside",
 
-                  data.label.show=false;
-                }else{
-                  return  `${data.name}`
-                }
-              },
-              show: true,
-              position: 'inside',
-
-              fontSize: 10,
-              fontFamily: 'Microsoft YaHei-Regular, Microsoft YaHei',
-              fontWeight: 400,
-              color: "black"
-
+            fontSize: 10,
+            fontFamily: "Microsoft YaHei-Regular, Microsoft YaHei",
+            fontWeight: 400,
+            color: "black",
           },
           itemStyle: {
             borderWidth: 1, // 外边框的宽度
@@ -164,18 +168,42 @@ function EpochView() {
             borderType: "solid", // 外边框的样式，默认为'solid'
           },
           data: [
-            { value: data[0][index], name: nameArray[0],label:{show:true} },
-            { value: data[1][index], name: nameArray[1] ,label:{show:true}},
-            { value: data[2][index], name: nameArray[2] ,label:{show:true}},
+            {
+              value: data[0][index],
+              name: nameArray[0],
+              label: { show: true },
+            },
+            {
+              value: data[1][index],
+              name: nameArray[1],
+              label: { show: true },
+            },
+            {
+              value: data[2][index],
+              name: nameArray[2],
+              label: { show: true },
+            },
           ],
         },
       ],
-    }
+    };
   };
   const color1 = ["#7cc380", "#81acda", "#f17f72"];
   const color2 = ["#f7be87", "#bbadcf", "#e2eec8"];
-  const pineOption2 = useMemo(() => {return sPineChartOption("reward_dis",color2,rewardNum,Index-1,["7","4","1"])}, [Index]);
-  const pineOption1 = useMemo(() => { return sPineChartOption("action_dis",color1,actionNum,Index-1,["NOOP","LEFT","RIGHT"])}, [Index]);
+  const pineOption2 = useMemo(() => {
+    return sPineChartOption("reward_dis", color2, rewardNum, Index - 1, [
+      "7",
+      "4",
+      "1",
+    ]);
+  }, [Index]);
+  const pineOption1 = useMemo(() => {
+    return sPineChartOption("action_dis", color1, actionNum, Index - 1, [
+      "NOOP",
+      "LEFT",
+      "RIGHT",
+    ]);
+  }, [Index]);
 
   const changeIndex = (e) => {
     console.log(e.target.value);
